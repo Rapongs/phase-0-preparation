1147324a7729ae9778c821c923873580b48efd35
Rapongs <gnopgnopar@gmail.com>
2026-04-09 15:39:04 +0700

    progres quiz rekursif

---
+++ quiz/Quiz-Rekursif-is-your-friend/soal4.js
@@ -0,0 +1,41 @@
+/*
+================
+Recursive - Hanya Bilangan Terbagi Nol
+================
+
+dividableRecursive adalah sebuah function yang
+menerima dua parameter berupa array dan num.
+function akan mereturn sebuah sebuah number yang 
+hanya sisa baginya nol dari bilangan num
+
+[RULE]
+- Wajib menggunakan recursive
+- Dilarang menggunakan regex .match dan lainnya!
+- Tidak boleh membuat variable diluar function
+- Tidak boleh menambahkan parameter baru
+- Tidak boleh menggunakan looping while/for    
+*/
+
+function dividableRecursive(array, num) {
+  //code here
+  if (array.length === 0) {
+    return "";
+  }
+
+  let currentElement = array[0];
+  let remainingArray = array.slice(1);
+
+  if (currentElement % num === 0) {
+    let nextResult = dividableRecursive(remainingArray, num);
+    
+    return currentElement + (nextResult ? " " + nextResult : "");
+  } else {
+    return dividableRecursive(remainingArray, num);
+  }
+}
+
+// DRIVER CODE
+console.log(dividableRecursive([66, 33, 55, 44, 11], 3)) // 66 33
+console.log(dividableRecursive([123, 222, 100, 50, 32], 2)) // 222 100 50 32
+console.log(dividableRecursive([125, 500, 201, 202, 66], 5)) // 125 500
+console.log(dividableRecursive([66, 33, 55, 44, 132], 6)) // 66 132
